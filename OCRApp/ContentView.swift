f1af4f605662c366d037011e57f9deb8fbd066ec
raduliano <radulian@me.com>
2022-04-18 12:25:44 +0300

    Initial Commit

---
+++ OCRApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OCRApp
+//
+//  Created by Octav Radulian on 18.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OCRApp/OCRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRAppApp.swift
+//  OCRApp
+//
+//  Created by Octav Radulian on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct OCRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
